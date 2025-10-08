2899ddaaafc6ddd42b2eb655253d3d1faea5f9d6
Rose Kay <HeyRoseKay@icloud.com>
2025-10-07 22:28:03 -0700

    Update Package.swift Dependency Links

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
             targets: ["Sliders"]),
     ],
     dependencies: [
-        .package(name: "bez", url: "https://github.com/liyb93/SwiftUI-bez.git", .branch("master")),
-        .package(url: "https://github.com/liyb93/CGExtender.git", .branch("master")),
-        .package(name: "Shapes", url: "https://github.com/liyb93/SwiftUI-Shapes.git", .branch("master"))
+        .package(url: "https://github.com/HeyRoseKay/CGExtender.git", .branch("master")),
+        .package(name: "bez", url: "https://github.com/HeyRoseKay/SwiftUI-bez.git", .branch("master")),
+        .package(name: "Shapes", url: "https://github.com/HeyRoseKay/SwiftUI-Shapes.git", .branch("master"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
